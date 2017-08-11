6c5c7538895152863ee333f702f7000309b07455
Egor Khmelev <hmelyoff@gmail.com>
2017-08-11 11:18:57 +0300

    Introduce support of overlayView for TabBar based apps

--- src/deprecated/platformSpecificDeprecated.ios.js
+++ src/deprecated/platformSpecificDeprecated.ios.js
@@ -131,6 +131,7 @@ async function startTabBasedApp(params) {
           style={params.tabsStyle}
           appStyle={params.appStyle}
           initialTabIndex={params.initialTabIndex}>
+          overlay={params.overlay}
           {
             params.tabs.map(function (tab, index) {
               return (
